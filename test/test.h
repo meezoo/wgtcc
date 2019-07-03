8cf09400a1e771741534d2423901a2411550ca56
meezoo <405641872@qq.com>
2019-07-02 21:12:57 -0700

    update test.h : exit on failure

--- test/test.h
+++ test/test.h
@@ -9,31 +9,35 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-#define fail(msg)																	\
-fprintf(stderr, "error:%s:%s:%d: failed, %s\n", 	\
-		__FILE__, __func__, __LINE__, (msg));					\
+#define fail(msg) do {                              \
+	fprintf(stderr, "error:%s:%s:%d: failed, %s\n", \
+			__FILE__, __func__, __LINE__, (msg));   \
+	exit(1);                                        \
+} while(0);
 
-#define expect(a, b)                            						\
-if ((a) != (b)) {                                   				\
-		fprintf(stderr, "error:%s:%s:%d: failed, %d != %d\n",  	\
-				__FILE__, __func__, __LINE__, (a), (b));						\
+#define expect(a, b)                            	      \
+if ((a) != (b)) {                                   	  \
+	fprintf(stderr, "error:%s:%s:%d: failed, %d != %d\n", \
+			__FILE__, __func__, __LINE__, (a), (b));      \
+	exit(1);											  \
+};
+
+#define expect_float(a, b)                          	  \
+if ((a) != (b)) {                                   	  \
+	fprintf(stderr, "error:%s:%s:%d: failed, %f != %f\n", \
+			__FILE__, __func__, __LINE__, (a), (b)); 	  \
+	exit(1);
+};
+
+#define expect_string(a, b)                         	  \
+if (strcmp((a), (b))) {    							      \
+	fprintf(stderr, "error:%s:%s:%d: failed, %s != %s\n", \
+			__FILE__, __func__, __LINE__, (a), (b)); 	  \
+	exit(1);
 };
 
 #define expectl expect
 #define expectf expect_float
 #define expectd expect_float
-
-#define expect_float(a, b)                          				\
-if ((a) != (b)) {                                   				\
-		fprintf(stderr, "error:%s:%s:%d: failed, %f != %f\n",  	\
-				__FILE__, __func__, __LINE__, (a), (b)); 						\
-};
-
-#define expect_string(a, b)                         				\
-if (strcmp((a), (b))) {  																		\
-		fprintf(stderr, "error:%s:%s:%d: failed, %s != %s\n",  	\
-				__FILE__, __func__, __LINE__, (a), (b)); 						\
-};
-
 
 #endif
